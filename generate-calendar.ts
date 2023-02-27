5431185b673680b386fc21297b91289f6a7ea815
Arjan Frans <arjan.frans@fusonic.net>
2023-02-27 11:16:56 +0100

    correct timezone

--- generate-calendar.ts
+++ generate-calendar.ts
@@ -1,12 +1,24 @@
 import ical from "ical-generator"
 import { readFileSync } from 'fs';
 import {parseDate} from "./src/date";
+import {getVtimezoneComponent} from '@touch4it/ical-timezones';
 
 const calendar = ical({name: 'BBL 2023'});
 
 
 const gameTableFile = readFileSync('./src/assets/bbl_2023.json');
 const gameTable = JSON.parse(gameTableFile.toString());
+
+function changeTimezone(date: Date, ianatz: string) {
+    const invdate = new Date(date.toLocaleString('en-US', {
+        timeZone: ianatz
+    }));
+
+    const diff = date.getTime() - invdate.getTime();
+
+    return new Date(date.getTime() - diff);
+
+}
 
 for (const entry of gameTable) {
     if (entry['Heim'] !== 'Hard Bulls' && entry['Gast'] !== 'Hard Bulls') {
@@ -26,12 +38,18 @@ for (const entry of gameTable) {
     const description = `${entry['Heim']} - ${entry['Gast']}`
     const summary = description;
 
+    calendar.timezone({
+        name: 'Europe/Vienna',
+        generator: getVtimezoneComponent
+    })
+
     calendar.createEvent({
-        start,
-        end,
+        start: changeTimezone(start, "UTC"),
+        end: changeTimezone(end, "UTC"),
         summary,
         description,
         location,
+        timezone: 'Europe/Vienna'
     });
 
     calendar.saveSync('./dist/bbl_2023.ics')
